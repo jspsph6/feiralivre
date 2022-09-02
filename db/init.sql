f9e4a4488e9f3634140bc30eb430c1730c4d0c3c
Jordan Pereira <jordan.pereira@sezzle.com>
2022-09-02 16:06:34 -0300

    database diagram, database creation sql, database docker compose yml

---
+++ db/init.sql
@@ -0,0 +1,46 @@
+USE fair;
+
+CREATE TABLE user (
+    id int not null auto_increment,
+    login varchar(255) not null,
+    hash varchar(255) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE district (
+    id int not null auto_increment,
+    code int not null,
+    code_subprefecture int not null,
+    areap int not null,
+    name varchar(255) not null,
+    region5 varchar(255) not null,
+    region8 varchar(255) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE fair (
+    id int not null auto_increment,
+    setcens int not null,
+    name varchar(255) not null,
+    register varchar(255) not null,
+    id_district int not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_district) REFERENCES district(id)
+);
+
+CREATE TABLE address (
+    id int not null auto_increment,
+    street varchar(255) not null,
+    number varchar(255) null,
+    neighborhood varchar(255) not null,
+    reference varchar(255) null,
+    coordinates point not null,
+    id_fair int not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_fair) REFERENCES fair(id)
+);
+
+SET character_set_client = utf8;
+SET character_set_connection = utf8;
+SET character_set_results = utf8;
+SET collation_connection = utf8_general_ci;
